5a65f1b44feb6a526b09bce2b636656912160b49
Hatalabdallah <hatalabdallah@gmail.com>
2022-10-15 13:48:01 +0300

    updated my answers

--- week9_assignment.py
+++ week9_assignment.py
@@ -13,94 +13,97 @@ from bs4 import BeautifulSoup
 from time import sleep
 from random import randint
 from openpyxl.workbook import Workbook
-# #Declaring the headers 
-# headers = {"Accept-Language": "en-US,en;q=0.5"}
 
-# #declaring the list of empty variables, So that we can append the data overall
+# Declaring the headers 
+headers = {"Accept-Language": "en-US,en;q=0.5"}
 
-# movie_name = []
-# year = []
-# time=[]
-# rating=[]
-# metascore =[]
-# votes = []
-# gross = []
-# description = []
-# Director = []
-# actors = []
-# Genre = []
+#declaring the list of empty variables, So that we can append the data overall
+
+movie_name = []
+year = []
+time=[]
+rating=[]
+metascore =[]
+votes = []
+gross = []
+description = []
+Director = []
+actors = []
+Genre = []
 
 
+#creating an array of values and passing it in the url for dynamic webpages
+pages = np.arange(1,9400584,50)
 
-# #creating an array of values and passing it in the url for dynamic webpages
-# pages = np.arange(1,9400584,50)
+# https://www.imdb.com/search/title/?year=1980-01-01,2022-12-31
+#the whole core of the script
+for page in pages:
+    page = requests.get("https://www.imdb.com/search/title/?year=1980-01-01,2022-12-31&start="+str(page)+"&ref_=adv_nxt")
+    soup = BeautifulSoup(page.text, 'html.parser')
+    movie_data = soup.findAll('div', attrs = {'class': 'lister-item mode-advanced'})
+    sleep(randint(2,8))
+    for store in movie_data:
+        name = store.h3.a.text
+        movie_name.append(name)
+        
+        year_of_release = store.h3.find('span', class_ = "lister-item-year text-muted unbold").text
+        year.append(year_of_release)
+        
+        runtime = store.p.find("span", class_ = 'runtime').text
+        time.append(runtime)
+        
+        rate = store.find('div', class_ = "inline-block ratings-imdb-rating").text.replace('\n', '')
+        rating.append(rate)
+        
+        meta = store.find('span', class_ = "metascore").text if store.find('span', class_ = "metascore") else "****"
+        metascore.append(meta)
+        
+        
+        value = store.find_all('span', attrs = {'name': "nv"})
+        
+        vote = value[0].text
+        votes.append(vote)
+        
+        grosses = value[1].text if len(value)>1 else '%^%^%^'
+        gross.append(grosses)
 
-# # https://www.imdb.com/search/title/?year=1980-01-01,2022-12-31
-# #the whole core of the script
-# for page in pages:
-#     page = requests.get("https://www.imdb.com/search/title/?year=1980-01-01,2022-12-31&start="+str(page)+"&ref_=adv_nxt")
-#     soup = BeautifulSoup(page.text, 'html.parser')
-#     movie_data = soup.findAll('div', attrs = {'class': 'lister-item mode-advanced'})
-#     sleep(randint(2,8))
-#     for store in movie_data:
-#         name = store.h3.a.text
-#         movie_name.append(name)
+        # Description of the Movies -- Not explained in the Video, But you will figure it out. 
+        describe = store.find_all('p', class_ = 'text-muted')
+        description_ = describe[1].text.replace('\n', '') if len(describe) >1 else '*****'
+        description.append(description_)
         
-#         year_of_release = store.h3.find('span', class_ = "lister-item-year text-muted unbold").text
-#         year.append(year_of_release)
+        #Cast Details -- Scraping Director name and Stars -- Not explained in Video
+        cast = store.find("p", class_ = '')
+        cast = cast.text.replace('\n', '').split('|')
+        cast = [x.strip() for x in cast]
+        cast = [cast[i].replace(j, "") for i,j in enumerate(["Director:", "Stars:"])]
+        Director.append(cast[0])
+        actors.append([x.strip() for x in cast[1].split(",")])
         
-#         runtime = store.p.find("span", class_ = 'runtime').text
-#         time.append(runtime)
-        
-#         rate = store.find('div', class_ = "inline-block ratings-imdb-rating").text.replace('\n', '')
-#         rating.append(rate)
-        
-#         meta = store.find('span', class_ = "metascore").text if store.find('span', class_ = "metascore") else "****"
-#         metascore.append(meta)
-        
-        
-#         value = store.find_all('span', attrs = {'name': "nv"})
-        
-#         vote = value[0].text
-#         votes.append(vote)
-        
-#         grosses = value[1].text if len(value)>1 else '%^%^%^'
-#         gross.append(grosses)
+        # Description of the Movies -- Not explained in the Video, But you will figure it out. 
+        describe = store.find_all('p', class_ = 'text-muted')
+        description_ = describe[1].text.replace('\n', '') if len(describe) >1 else '*****'
+        description.append(description_)
 
-#         # Description of the Movies -- Not explained in the Video, But you will figure it out. 
-#         describe = store.find_all('p', class_ = 'text-muted')
-#         description_ = describe[1].text.replace('\n', '') if len(describe) >1 else '*****'
-#         description.append(description_)
-        
-#         #Cast Details -- Scraping Director name and Stars -- Not explained in Video
-#         cast = store.find("p", class_ = '')
-#         cast = cast.text.replace('\n', '').split('|')
-#         cast = [x.strip() for x in cast]
-#         cast = [cast[i].replace(j, "") for i,j in enumerate(["Director:", "Stars:"])]
-#         Director.append(cast[0])
-#         actors.append([x.strip() for x in cast[1].split(",")])
-        
-#         # Description of the Movies -- Not explained in the Video, But you will figure it out. 
-#         describe = store.find_all('p', class_ = 'text-muted')
-#         description_ = describe[1].text.replace('\n', '') if len(describe) >1 else '*****'
-#         description.append(description_)
-
-#         genre = store.p.find("span", class_ = 'genre').text
-#         Genre.append(genre)
+        genre = store.p.find("span", class_ = 'genre').text
+        Genre.append(genre)
 
         
-# #creating a dataframe 
-# movie_list = pd.DataFrame({ "Movie Name": movie_name, "Year of Release" : year, "Watch Time": time,"Movie Rating": rating, "Meatscore of movie": metascore,
-#  "Votes" : votes, "Gross": gross, "Description": description, "Director": Director, "Actor": actors, 'Genre': genre  })
+#creating a dataframe 
+movie_list = pd.DataFrame({ "Movie Name": movie_name, "Year of Release" : year, "Watch Time": time,"Movie Rating": rating, "Meatscore of movie": metascore,
+ "Votes" : votes, "Gross": gross, "Description": description, "Director": Director, "Actor": actors, 'Genre': genre  })
 # movie_list.head(5)
 
 # # #saving the data in excel format
 # movie_list.to_excel("Top 1000 IMDb movies.xlsx")
 
-# # #If you want to save the data in csv format
-# movie_list.to_csv("Top 1000 IMDb movies.csv")
+# #If you want to save the data in csv format
+movie_list.to_csv("Top 1000 IMDb movies.csv")
 
-# print("done")
+print("done")
+
+
+
 
 # Scrap Hacker News project and save the result in a csv file. The csv file should have the 
 # following columns: title, link, points, comments, author, rank. The csv file should be sorted 
@@ -108,85 +111,58 @@ from openpyxl.workbook import Workbook
 
 headers = {"Accept-Language": "en-US,en;q=0.5"}
 
-#declaring the list of empty variables, So that we can append the data overall
-
-Title = []
-Link = []
-Points=[]
-Comments=[]
-Author =[]
-Rank = []
+Titles = []
+Links = []
+Points = []
+Comments = []
+Authors = []
+Ranks = []
 
 url = 'https://news.ycombinator.com/'
-page = range(1,26)
-r = requests.get(f'https://news.ycombinator.com/news?p={page}')
-soup = BeautifulSoup(r.content, 'html.parser')
 
-data = soup.find_all(class_ = "athing")
-print(data)
-# #creating an array of values and passing it in the url for dynamic webpages
+for page in range(1,27):
+    r = requests.get(f'https://news.ycombinator.com/news?p={page}')
 
-# # https://www.imdb.com/search/title/?year=1980-01-01,2022-12-31
-# #the whole core of the script
-# for page in pages:
-#     page = requests.get("https://www.imdb.com/search/title/?year=1980-01-01,2022-12-31&start="+str(page)+"&ref_=adv_nxt")
-#     soup = BeautifulSoup(page.text, 'html.parser')
-#     movie_data = soup.findAll('div', attrs = {'class': 'lister-item mode-advanced'})
-#     sleep(randint(2,8))
-#     for store in movie_data:
-#         name = store.h3.a.text
-#         movie_name.append(name)
-        
-#         year_of_release = store.h3.find('span', class_ = "lister-item-year text-muted unbold").text
-#         year.append(year_of_release)
-        
-#         runtime = store.p.find("span", class_ = 'runtime').text
-#         time.append(runtime)
-        
-#         rate = store.find('div', class_ = "inline-block ratings-imdb-rating").text.replace('\n', '')
-#         rating.append(rate)
-        
-#         meta = store.find('span', class_ = "metascore").text if store.find('span', class_ = "metascore") else "****"
-#         metascore.append(meta)
-        
-        
-#         value = store.find_all('span', attrs = {'name': "nv"})
-        
-#         vote = value[0].text
-#         votes.append(vote)
-        
-#         grosses = value[1].text if len(value)>1 else '%^%^%^'
-#         gross.append(grosses)
+    soup = BeautifulSoup(r.content, 'html.parser')
 
-#         # Description of the Movies -- Not explained in the Video, But you will figure it out. 
-#         describe = store.find_all('p', class_ = 'text-muted')
-#         description_ = describe[1].text.replace('\n', '') if len(describe) >1 else '*****'
-#         description.append(description_)
-        
-#         #Cast Details -- Scraping Director name and Stars -- Not explained in Video
-#         cast = store.find("p", class_ = '')
-#         cast = cast.text.replace('\n', '').split('|')
-#         cast = [x.strip() for x in cast]
-#         cast = [cast[i].replace(j, "") for i,j in enumerate(["Director:", "Stars:"])]
-#         Director.append(cast[0])
-#         actors.append([x.strip() for x in cast[1].split(",")])
-        
-#         # Description of the Movies -- Not explained in the Video, But you will figure it out. 
-#         describe = store.find_all('p', class_ = 'text-muted')
-#         description_ = describe[1].text.replace('\n', '') if len(describe) >1 else '*****'
-#         description.append(description_)
+    data = soup.find_all('tr', class_ = "athing")
+    data2 = soup.find_all('td', class_ = 'subtext')
 
-#         genre = store.p.find("span", class_ = 'genre').text
-#         Genre.append(genre)
+
+    for item in data:
+        Title = item.find('span', class_ = "titleline").find('a').text
+        Titles.append(Title)
+
+        Link = item.find('span', class_ = 'titleline').find('a', href=True)
+        Links.append(Link)
+
+        Rank = item.find('span', class_ = 'rank').text.strip('.')
+        Ranks.append(Rank)
 
         
-# #creating a dataframe 
-# movie_list = pd.DataFrame({ "Movie Name": movie_name, "Year of Release" : year, "Watch Time": time,"Movie Rating": rating, "Meatscore of movie": metascore,
-#  "Votes" : votes, "Gross": gross, "Description": description, "Director": Director, "Actor": actors, 'Genre': genre  })
-# movie_list.head(5)
 
-# # #saving the data in excel format
-# movie_list.to_excel("Top 1000 IMDb movies.xlsx")
+    for item in data2:
+        if item.find('span', class_ = 'score') is not None:
+            Point = item.find('span', class_ = 'score').text
+            Points.append(Point)
 
-# # #If you want to save the data in csv format
-# movie_list.to_csv("Top 1000 IMDb movies.csv")
+        else:
+            pass
+
+        if item.find_all('a')[-1].text is not None:
+            Comment = item.find_all('a')[-1].text
+            Comments.append(Comment)
+
+        
+        if item.find('a', class_ = "hnuser") is not None:
+            Author = item.find('a', class_ = "hnuser").text
+            Authors.append(Author)
+
+
+data = pd.DataFrame({ "Title": Titles, "Link" : Links, "Rank": Ranks})
+
+data2 = pd.DataFrame({"Point": Points, "Comment": Comments,"Author" : Authors })
+
+data.to_csv("Hacker News.csv")
+data2.to_csv("Hacker News.csv")
+
